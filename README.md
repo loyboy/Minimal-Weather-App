c9d27cfca277bd453f080615cafe52800530bcd2
Emmanuel Etukudo <emmaetuk@outlook.com>
2018-11-27 15:46:49 +0000

    final commit for Openweather app

---
+++ README.md
@@ -0,0 +1,26 @@
+# OpenWeather App
+
+## Project Description
+
+Its a minimal App that displays the Weather in location (Cities) across the World. App was developed using Node.js (Express)
+Advanced version will come soon.
+  
+## Build With
+
+  * HTML
+  * CSS
+  * JavaScript
+  * Node.js
+  * Express
+  
+## Author
+
+This [Openweather App](https://loyweather-app.herokuapp.com/) was developed by Loyboy.
+
+## Project Link
+
+To access this project on the web, click on this link: [OpenWeather App](https://loyweather-app.herokuapp.com/) and view in any web browser such as [Edge](https://www.microsoft.com/en-us/download/details.aspx?id=48126), [Firefox](https://www.mozilla.org/en-US/firefox/new/), or [Chrome](https://support.google.com/chrome/answer/95346?hl=en) etc
+
+## Contributing
+
+This project is open to contribution and improvement. You are free for fork it and improve on it.
---
+++ server.js
@@ -0,0 +1,54 @@
+// Require Express
+const express = require('express');
+// Require Body Parser
+const bodyParser = require('body-parser');
+// Require Request
+const request = require('request');
+
+const PORT = process.env.PORT || 5000
+
+// require and load dotenv
+require('dotenv').load();
+
+//Instantiating our App
+const app = express()
+
+// Using our API Key
+const apiKey = '569b3a8f833669ae7a1847adb5ce05bc';
+
+app.use(express.static('public'));
+
+// Use Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Setting our Template Engine View to EJS
+app.set('view engine', 'ejs');
+// Getting our Root URL
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
+
+// Form Route for Post Request
+app.post('/', function (req, res) {
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+  
+    // utilizing Request Module
+    request(url, function (err, response, body) {
+      if(err){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        // Parse the data
+        let weather = JSON.parse(body)
+        if(weather.main == undefined){
+          res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+          let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+          res.render('index', {weather: weatherText, error: null});
+        }
+      }
+    });
+  })
+
+// Server Port
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
